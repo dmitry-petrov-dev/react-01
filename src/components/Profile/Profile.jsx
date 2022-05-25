f856cfb57203ac427c62e5e1b7bd865672c156c3
DmitryPetrov <petroff.dmitry@gmail.com>
2022-05-25 09:48:44 +0300

    19

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import classes from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <img
         src="https://www.gettyimages.pt/gi-resources/images/Homepage/Hero/PT/PT_hero_42_153645159.jpg"
         alt="picture"
